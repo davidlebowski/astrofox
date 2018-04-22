7e44e5f1fc94b471d43bfdd55bb3ca3ff65f1f53
davide <davide.spicuglia@gmail.com>
2018-04-21 18:54:14 -0700

    Added feedback FX when thrust is applied

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -10,40 +10,65 @@ namespace Astrofox
 	// multiplayer support.
 	public class PlayerController
 	{
-		public Actor PlayerActor { set; get; }
-		private readonly Weapon m_playerWeapon;
+		public Actor PlayerActor
+		{
+			set
+			{
+				m_playerActor = value;
+				m_playerWeapon = m_playerActor.GetComponent<Weapon>();
+				m_playerVisuals = m_playerActor.GetComponent<PlayerShipVisuals>();
+			}
+			get
+			{
+				return m_playerActor;
+			}
+		}
+		private Weapon m_playerWeapon;
+		private PlayerShipVisuals m_playerVisuals;
+		private Actor m_playerActor;
 
 		public PlayerController(Actor playerActor)
 		{
 			PlayerActor = playerActor;
-			m_playerWeapon = PlayerActor.GetComponent<Weapon>();
 		}
 
 		public void Update()
 		{
-			if (PlayerActor == null || PlayerActor.IsDead)
+			if (m_playerActor == null || m_playerActor.IsDead)
 			{
 				return;
 			}
+			Vector3 thrustersDirection = Vector3.zero;
+			bool thrustersEnabled = false;
 			if (Input.GetKey(KeyCode.A))
 			{
-				PlayerActor.transform.Rotate(Vector3.forward * Systems.GameConfig.PlayerRotationSpeed * Time.deltaTime, Space.World);
+				m_playerActor.transform.Rotate(Vector3.forward * Systems.GameConfig.PlayerRotationSpeed * Time.deltaTime, Space.World);
 			}
 			else if (Input.GetKey(KeyCode.D))
 			{
-				PlayerActor.transform.Rotate(-Vector3.forward * Systems.GameConfig.PlayerRotationSpeed * Time.deltaTime, Space.World);
+				m_playerActor.transform.Rotate(-Vector3.forward * Systems.GameConfig.PlayerRotationSpeed * Time.deltaTime, Space.World);
 			}
 			if (Input.GetKey(KeyCode.W))
 			{
-				PlayerActor.AddForce(PlayerActor.Forward * Systems.GameConfig.PlayerForwardThrust);
+				thrustersEnabled = true;
+				thrustersDirection = m_playerActor.Forward;
+				m_playerActor.AddForce(thrustersDirection * Systems.GameConfig.PlayerForwardThrust);
 			}
 			else if (Input.GetKey(KeyCode.S))
 			{
-				PlayerActor.AddForce(-PlayerActor.Forward * Systems.GameConfig.PlayerBackwardThrust);
+				thrustersEnabled = true;
+				thrustersDirection = -m_playerActor.Forward;
+				m_playerActor.AddForce(thrustersDirection * Systems.GameConfig.PlayerBackwardThrust);
 			}
 			if (m_playerWeapon && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
 			{
-				m_playerWeapon.Shoot(PlayerActor.Forward);
+				m_playerWeapon.Shoot(m_playerActor.Forward);
+			}
+
+			if (m_playerVisuals != null)
+			{
+				m_playerVisuals.SetThrustersState(thrustersEnabled);
+				m_playerVisuals.SetThrustersDirection(-thrustersDirection);
 			}
 		}
 	}
---
+++ Assets/Code/PlayerShipVisuals.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Astrofox
+{
+	public class PlayerShipVisuals : MonoBehaviour
+	{
+		[SerializeField] private ParticleSystem m_thrusters;
+
+		public void SetThrustersDirection(Vector3 direction)
+		{
+			if (m_thrusters != null)
+			{
+				m_thrusters.transform.forward = direction;
+			}
+		}
+
+		public void SetThrustersState(bool enabled)
+		{
+			if (m_thrusters != null)
+			{
+				ParticleSystem.EmissionModule emissionModule = m_thrusters.emission;
+				emissionModule.enabled = enabled;
+			}
+		}
+	}
+}
